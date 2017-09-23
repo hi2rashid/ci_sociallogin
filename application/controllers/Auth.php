1e09d5eb2634862b1b4e4b9c8e5d35404fea1092
Abdul Rashid <hi2rashid@gmail.com>
2017-09-23 23:45:05 +0300

    Facebook login done

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -18,6 +18,12 @@ class Auth extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	 function __construct(){
+		parent::__construct();
+		$this->load->config("oauth");
+		$this->load->library("Facebook");
+	 }
 	public function index()
 	{
 		$this->load->view('welcome_message');
@@ -25,5 +31,60 @@ class Auth extends CI_Controller {
 
 	public function login(){
 		
+
+		$userData = array();
+		$data['fb_authUrl'] = "#";
+				// Check if user is logged in
+				if($this->facebook->is_authenticated()){
+					// Get user facebook profile details
+					$userProfile = $this->facebook->request('get', '/me?fields=id,first_name,last_name,email,gender,locale,picture');
+		
+					// Preparing data for database insertion
+					$userData['oauth_provider'] = 'facebook';
+					$userData['oauth_uid'] = $userProfile['id'];
+					$userData['first_name'] = $userProfile['first_name'];
+					$userData['last_name'] = $userProfile['last_name'];
+					$userData['email'] = $userProfile['email'];
+					$userData['gender'] = $userProfile['gender'];
+					$userData['locale'] = $userProfile['locale'];
+					$userData['profile_url'] = 'https://www.facebook.com/'.$userProfile['id'];
+					$userData['picture_url'] = $userProfile['picture']['data']['url'];
+		print_r($userData);
+					// Insert or update user data
+					$userID = 0;//$this->user->checkUser($userData);
+		
+					// Check user data insert or update status
+					if(!empty($userID)){
+						$data['userData'] = $userData;
+						$this->session->set_userdata('userData',$userData);
+					}else{
+					   $data['userData'] = array();
+					}
+		
+					// Get logout URL
+					$data['logoutUrl'] = $this->facebook->logout_url();
+				}else{
+					$fbuser = '';
+		
+					// Get login URL
+					$data['fb_authUrl'] =  $this->facebook->login_url();
+				}
+				print_r($data);
+		$this->load->view("auth/login",$data);
+	}
+
+	public function facebook_logout(){
+			// Remove local Facebook session
+			$this->facebook->destroy_session();
+	
+			// Remove user data from session
+			$this->session->unset_userdata('userData');
+	
+			// Redirect to login page
+			redirect('/user_authentication');
+	}
+
+	public function facebook_authentication(){
+		print_r($_REQUEST);
 	}
 }
---
+++ application/views/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php $asset_path = $this->config->item('asset_path'); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url($asset_path . "/bootstrap/dist/css/bootstrap.min.css"); ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+
+    
+ 
+  </head>
+
+  <body>
